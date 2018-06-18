add9801c1d08c06261acac2b18fb002cd545e902
binoyskumar92 <subhakum@usc.edu>
2018-06-18 13:08:30 -0700

    added embed video

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
-import React,{Component} from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar'
+import VideoDetail from './components/video_detail'
 import VideoList from './components/video_list'
 import YTSearch from 'youtube-api-search'
 
-const API_KEY="AIzaSyAfv7Z-f7K-oFIr3v1bcOozXFfqgwDP56Q";
+
+const API_KEY = "AIzaSyAfv7Z-f7K-oFIr3v1bcOozXFfqgwDP56Q";
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={videos:[]};
-    YTSearch({key: API_KEY,term: 'surfboards'},(videos) => {
-      this.setState({videos})
-    })
-    this.state={videos:[]};
-  }
+    constructor(props) {
+        super(props);
+        this.state = {videos: []};
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({videos})
+        })
+        this.state = {videos: []};
+    }
 
-  render(){
-    return (
-    <div className="panel panel-default">
-    <SearchBar/>
-    <VideoList videos={this.state.videos}/>
-    </div>);
-  }
+    render() {
+        return (
+            <div className="panel panel-default">
+                <SearchBar/>
+                <VideoDetail video={this.state.videos[0]}/>
+                <VideoList videos={this.state.videos}/>
+            </div>);
+    }
 }
-ReactDOM.render(<App/>,document.querySelector('.container'));
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
