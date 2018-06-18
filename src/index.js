42798414bd902165548bf55c0a8d8439e5b1733b
binoyskumar92 <subhakum@usc.edu>
2018-06-17 19:29:53 -0700

    Added a plain input

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar'
+const API_KEY="AIzaSyAfv7Z-f7K-oFIr3v1bcOozXFfqgwDP56Q";
 const App=function() {
-  return <div>Hi!</div>;
+  return (
+    <div>
+    <SearchBar/>
+    </div>);
 }
 ReactDOM.render(<App/>,document.querySelector('.container'));
