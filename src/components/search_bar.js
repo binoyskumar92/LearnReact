ef2ebc011f0dd07e33f8b3569440f478f0d0abd2
binoyskumar92 <subhakum@usc.edu>
2018-06-17 19:57:17 -0700

    Conole inputchanges

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,16 @@
-import React from 'react'
-const SearchBar= ()=>{
-  return (
-    <div>
-    <input type="text"/>
-    </div>)
+import React,{ Component } from 'react'
+// const SearchBar= ()=>{
+//   return (
+//     <div>
+//     <input type="text"/>
+//     </div>)
+// }
+class SearchBar extends Component{
+  render(){
+    return <input onChange={this.onInputChange}/>;
+  }
+  onInputChange(event){
+    console.log(event.target.value);
+  }
 }
 export default SearchBar;
