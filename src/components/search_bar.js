0d07408cd7fb7317a10d66f8c3e4a095c3610b17
binoyskumar92 <subhakum@usc.edu>
2018-06-17 20:37:57 -0700

    Controlled input with value setting by using state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,8 +6,14 @@ import React,{ Component } from 'react'
 //     </div>)
 // }
 class SearchBar extends Component{
+  constructor(props){
+    super(props);
+    this.state={ term: ''};
+  }
   render(){
-    return <input onChange={(event) => console.log(event.target.value)}/>;
+    return <input
+    value={this.state.term}
+    onChange={event => this.setState({term: event.target.value})}/>;
   }
 
 }
