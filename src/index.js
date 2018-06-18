7e32586234dca8369e787f36ec5944102a647c77
binoyskumar92 <subhakum@usc.edu>
2018-06-18 00:43:20 -0700

    Set state in parent and trasferred values to children using props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React,{Component} from 'react'
 import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import YTSearch from 'youtube-api-search'
+
 const API_KEY="AIzaSyAfv7Z-f7K-oFIr3v1bcOozXFfqgwDP56Q";
-const App=function() {
-  return (
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={videos:[]};
+    YTSearch({key: API_KEY,term: 'surfboards'},(videos) => {
+      this.setState({videos})
+    })
+    this.state={videos:[]};
+  }
+
+  render(){
+    return (
     <div>
     <SearchBar/>
+    <VideoList videos={this.state.videos}/>
     </div>);
+  }
 }
 ReactDOM.render(<App/>,document.querySelector('.container'));
