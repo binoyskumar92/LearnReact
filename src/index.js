45ea9c0f5698660b18a5570bb9bb13a6b031749a
binoyskumar92 <subhakum@usc.edu>
2018-06-23 17:32:05 -0700

    Adde styling and throttling to youtube search

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import SearchBar from './components/search_bar'
 import VideoDetail from './components/video_detail'
 import VideoList from './components/video_list'
 import YTSearch from 'youtube-api-search'
-
+import _ from 'lodash';
 
 const API_KEY = "AIzaSyAfv7Z-f7K-oFIr3v1bcOozXFfqgwDP56Q";
 
@@ -15,19 +15,24 @@ class App extends Component {
             videos: [],
             selectedVideo: null
         };
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({
-                videos: videos,
-                selectedVideo: videos[0]
-            })
-        })
-        this.state = {videos: []};
-    }
 
+        this.state = {videos: []};
+        this.videoSearch('surfboards')
+    }
+    videoSearch(term){
+      YTSearch({key: API_KEY, term: term}, (videos) => {
+          this.setState({
+              videos: videos,
+              selectedVideo: videos[0]
+          })
+      })
+    }
     render() {
+      const videoSearch = _.debounce(term=>{this.videoSearch(term)},300);
+
         return (
-            <div>
-                <SearchBar/>
+          <div>
+                <SearchBar onSearchTermChange={videoSearch}/>
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
